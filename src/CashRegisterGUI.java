475d1b8d367eda4ec79b8194f7ad6fb267755e28
Darren-C26 <darren.constantine@ontariotechu.net>
2022-10-26 02:06:04 -0400

    Update CashRegisterGUI.java

--- src/CashRegisterGUI.java
+++ src/CashRegisterGUI.java
@@ -1,20 +1,83 @@
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class CashRegisterGUI {
-	JFrame jf;
-	public CashRegisterGUI() {
-		jf = new JFrame("Display");
-		
-		JButton btn = new JButton("Start New Session");		
-		jf.add(btn);
-		jf.setLayout(new FlowLayout());
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setSize(400, 400);
-		jf.setVisible(true);
-	}
-	
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		new CashRegisterGUI();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CashRegisterGUI window = new CashRegisterGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
+
+	/**
+	 * Create the application.
+	 */
+	public CashRegisterGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));	
+		contentPane.setBackground(Color.darkGray);
+		JButton btn = new JButton("Start New Sesson");
+		btn.addMouseListener(new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+				BarcodeScanner scanFrame = new BarcodeScanner();
+				frame.setVisible(false);
+				scanFrame.setVisible(true);
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		}
+		);
+		contentPane.add(btn);
+		frame.setContentPane(contentPane);
+	}
+
 }
