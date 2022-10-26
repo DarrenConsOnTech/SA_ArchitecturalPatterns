10c185e8a2d2a3324e44b57cd6ed755a8827f2c0
Darren-C26 <darren.constantine@ontariotechu.net>
2022-10-26 02:06:01 -0400

    Update BarcodeScanner.java

--- src/BarcodeScanner.java
+++ src/BarcodeScanner.java
@@ -1,9 +1,35 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
-public class BarcodeScanner {
+public class BarcodeScanner extends JFrame {
 
+	private JPanel contentPane;
+
+/*
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BarcodeScanner frame = new BarcodeScanner();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+*/
 
+	public BarcodeScanner() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.darkGray);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		JButton btn = new JButton("Scan Item");		
+		contentPane.add(btn);
+		setContentPane(contentPane);
 	}
 
 }
